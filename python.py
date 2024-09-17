7631fe2a2eb1cda7a0641cd7be4e00f432491171
keerthana saravanan <keerthanasaravanan008@gmail.com>
2024-09-17 07:42:41 -0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,13 @@
+#apptitude test
+
+# print output
+
+print ("hello world")
+
+# addition & division of arthimetic opertion
+
+num1= float(input("enter the first num:"))
+num2= float(input("enter the second num:"))
+result=num1+num2
+print("sum:{num1}+{num2}={result}")
+
