7e7f149e4b5d2e01f32673bd5b445037b4e60134
keerthana saravanan <keerthanasaravanan008@gmail.com>
2024-09-17 08:04:15 -0700

    Create java.java

---
+++ java.java
@@ -0,0 +1,18 @@
+//reverse the string
+public class ReverseString{
+  public static void main(string[]args){
+    string str="Automation";
+    stringbuilder reverse=new stringBuilder(str).reverse();
+    system.out.print\n(reverse);
+  }
+}
+ //sort an array
+
+import java.util.Arrays;
+public class SortArray{
+  public static void main(String[]args){
+    int[]arr={1,2,3,5,6}
+    array.sort(arr);
+    system.out.print\n(Array.toSring(arr));
+  }
+}
